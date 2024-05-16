ad8b1f2f8399ae2557186497ce05a5dea42d8b36
KBSLimited <jdeandre1220@gmail.com>
2024-05-16 00:55:59 -0700

    Save point

---
+++ 1. Showcase Your Dance Moves!.py
@@ -0,0 +1,21 @@
+#Task 1: Code Correction
+
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+#Task 2: Your Mood Today
+
+mood = input("How are you feeling today? Happy or Sad?")
+
+mood = ("Happy", "Sad")
+if "Happy" in mood:
+  print("That's good to hear!")
+else:
+   if "Sad" in mood:
+      print("I hope your day gets better!")
+
+
