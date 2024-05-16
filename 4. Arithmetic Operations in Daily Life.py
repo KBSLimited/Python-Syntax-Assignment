8475e23952c7bdf5948cada064dd0805f0bd7c7b
KBSLimited <jdeandre1220@gmail.com>
2024-05-16 02:49:09 -0700

    update

--- 4. Arithmetic Operations in Daily Life.py
+++ 4. Arithmetic Operations in Daily Life.py
@@ -1,6 +1,16 @@
 #Task 1: Grocery Store Math
+x = 8.65
+y = 4.99
+z = 3.50
 
-
-
+print(x + y + z)
 
 #Task 2: Bank Interest
+P = 10000
+n = 4
+r = 8.00
+y = 1
+
+FV = P * (((1 + ((r/100.0)/n)) ** (n*y)))
+
+print ("The final amount after", y, "years is", FV)
