98862fca07cc1b772816c27923771f0c3db92198
KBSLimited <jdeandre1220@gmail.com>
2024-05-16 18:13:04 -0700

    Save Point

--- 6. Exploring Logical Operations and Precedence.py
+++ 6. Exploring Logical Operations and Precedence.py
@@ -1,6 +1,15 @@
 #Task 1: Validating Calculations
+x = 5 + 3
 
+print(x)
 
+y = (5 + 3)
 
+print(y)
 
 #Task 2: Mix and Match
+x = 5
+y = 3
+
+print((x + y > x - y) or (x * y > x % y))
+#My prediction is: True.
